b2def5e334014f7adf5a8beb07f76e1bd5e80fa6
laurence <laurence.shann@gmail.com>
2024-01-10 23:46:59 +0000

    added personal site (laurence)

---
+++ laurence/compatibility.js
@@ -0,0 +1,8 @@
+function checkCompatibility(){
+    // detect CSS text-shadow support in JavaScript
+    if (document.createElement("detect").style.textShadow === "") {
+        alert("supported");
+
+        document.getElementsByClassName("item")[]
+    }
+}
